3318217d6d9f83b2e51450afd0f812bbba6c2c2f
Kachit <antoxachaos@gmail.com>
2018-06-29 13:32:57 +0300

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Angular API query builder module
 Angular API query builder module description
+
+```js
+let qb = new QueryBuilderService();
+let params = qb.eq('active', true).build();
+```
--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,7 @@
 export * from './src/app/api-query-builder-module';
+export * from './src/app/api-query-builder-module/query-builder.service';
+export * from './src/app/api-query-builder-module/query-builder.interface';
+export * from './src/app/api-query-builder-module/query-builder.provider';
+export * from './src/app/api-query-builder-module/query-builder-field.enum';
+export * from './src/app/api-query-builder-module/query-builder-operator.enum';
+export * from './src/app/api-query-builder-module/query-builder-order.enum';
