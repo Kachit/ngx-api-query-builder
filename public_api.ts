720b403e9bf1bd0455917ea3445f402ad26e3190
Kachit <antoxachaos@gmail.com>
2019-01-19 13:26:24 +0300

    Clear

--- public_api.ts
+++
@@ -1,7 +0,0 @@
-export * from './src/app/api-query-builder-module/api-query-builder.module';
-export * from './src/app/api-query-builder-module/query-builder.service';
-export * from './src/app/api-query-builder-module/query-builder.interface';
-export * from './src/app/api-query-builder-module/query-builder.provider';
-export * from './src/app/api-query-builder-module/query-builder-field.enum';
-export * from './src/app/api-query-builder-module/query-builder-operator.enum';
-export * from './src/app/api-query-builder-module/query-builder-order.enum';
--- src/app/api-query-builder-module/query-builder-field.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum QueryBuilderField {
-    FILTER = 'filter',
-    ORDER = 'order',
-    LIMIT = 'limit',
-    OFFSET = 'offset',
-    INCLUDE = 'include',
-}
